47626173ac079c30d93231cbc59b5a43b0006036
Prajwalit Bhopale <prajwalit@gmail.com>
2011-09-15 21:26:18 +0530

    Additional changes to js2-mode

--- config/js2-mode.el
+++ config/js2-mode.el
@@ -12,4 +12,14 @@
                                                        (match-end 1) "ƒ")
                                        nil)))))))
 
+(defun turn-on-paredit ()
+  (paredit-mode t))
+
+(defun fix-tab-width-for-js2-mode ()
+  (setq-default tab-width 2)
+  (setq js2-basic-offset 2))
+
+(add-hook 'js2-mode-hook 'turn-on-paredit)
+(add-hook 'js2-mode-hook 'fix-tab-width-for-js2-mode)
+
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
