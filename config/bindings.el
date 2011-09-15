e6e75fc5860cee3573af0fce6efaf55ad18640f1
Prajwalit Bhopale <prajwalit@gmail.com>
2011-09-15 21:26:36 +0530

    Added my bindings. removed punish-mode

--- config/bindings.el
+++ config/bindings.el
@@ -4,7 +4,7 @@
 (global-set-key "\M-w" 'clipboard-kill-ring-save)
 (global-set-key "\C-y" 'clipboard-yank)
 
-(global-set-key "\C-c\M-t" 'multi-term)
+(global-set-key (kbd "C-x m") 'multi-term)
 (global-set-key (kbd "C-c t") 'multi-term-next)
 (global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
 
@@ -14,10 +14,13 @@
 (global-set-key "\C-xk" 'kill-current-buffer)
 (global-set-key (kbd "C-c y") 'bury-buffer)
 
-(global-set-key (kbd "<up>") 'punish-me)
-(global-set-key (kbd "<down>") 'punish-me)
-(global-set-key (kbd "<left>") 'punish-me)
-(global-set-key (kbd "<right>") 'punish-me)
+(global-set-key (kbd "C-x r") 'rgrep)
+(global-set-key (kbd "M-g") 'goto-line)
+
+;; (global-set-key (kbd "<up>") 'punish-me)
+;; (global-set-key (kbd "<down>") 'punish-me)
+;; (global-set-key (kbd "<left>") 'punish-me)
+;; (global-set-key (kbd "<right>") 'punish-me)
 
 
 (eval-after-load "magit"
